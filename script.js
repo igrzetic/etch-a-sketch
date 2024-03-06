9b46b319d5816710834f62496916eb6178d99a6b
Ivan <130054944+igrzetic@users.noreply.github.com>
2024-03-06 18:46:07 +0100

    Funkcija za clear, eraser i ranbow funkcionalne

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ function populateBoard(size) {
   for (let i = 0; i < amount; i++) {
     let square = document.createElement("div");
     square.addEventListener("mouseover", colorSquare);
-    board.insertAdjacentElement("beforeend", square);
+    board.append(square);
   }
 }
-
 populateBoard(16);
 
+// EVENT LISTENER ZA GUMB ZA PROMJENU VELIČINE
 const sizeBtn = document.getElementById('sizeBtn');
 sizeBtn.addEventListener('click', function() {
     let newSize = document.querySelector('.input').value;
@@ -22,37 +22,42 @@ sizeBtn.addEventListener('click', function() {
     console.log("new size: " + newSize);
 });
 
+//FUNKCIJA ZA PROMJENU VELIČINE KVADRATIĆA
 function changeSize(newSize) {
   if (newSize >= 2 && newSize <= 100) {
     document.querySelector(".error").style.display = "none";
     populateBoard(newSize);
   } else {
-    document.querySelector(".error").style.display = "flex";
+    document.querySelector(".error").style.display = "block";
   }
 }
 
 // FUNKCIJA KOJA MI BOJA DIV
 let color = "black";
+const colorBtn = document.getElementById('colorBtn');
+const rainbowBtn = document.getElementById('rainbowBtn');
+const eraserBtn = document.getElementById('eraserBtn');
+const clearBtn = document.getElementById('clearBtn');
 
 function colorSquare() {
   if (click) {
-    // if (color === "random") {
-    //   this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-    // } else {
+    if (color === "rainbow") {
+      this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
       this.style.backgroundColor = color;
-    // }
+    }
   }
 }
 
-// function changeColor(choice) {
-//   color = choice;
-// }
+function changeColor(choice) {
+  color = choice;
+}
 
-// function resetBoard() {
-//   let board = document.querySelector(".board");
-//   let squares = board.querySelectorAll("div");
-//   squares.forEach((div) => (div.style.backgroundColor = "white"));
-// }
+function clearBoard() {
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => (div.setAttribute('style', 'backgoundColor: "white"; borderRadius = 20px; boxShadow: inset 0 0 7.5px rgba(0, 0, 0, 0.5)')))
+}
 
 // FUNKCIJA KADA KLIKNEM NA BODY DA MI PRESTANE CRTATI
 let click = true;
